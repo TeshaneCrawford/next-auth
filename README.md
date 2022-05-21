43cdcdff3da163da55a1ef8b6d7269afae2d36c1
Teshane Crawford <crawfordteshane@gmail.com>
2022-05-20 19:07:05 -0500

    setup firebase adapter 1.0

--- README.md
+++ README.md
@@ -12,6 +12,7 @@ Css styling done with Tailwind, Heroicons & Tailwind forms
 
 Installation guides: https://tailwindcss.com/docs/guides/nextjs
 # firebase adapter
+The firebase adapter is experimental and not yet ready for production use.
 
 Installation using yarn: yarn add next-auth @next-auth/firebase-adapter@experimental
 Installation using npm: npm install next-auth @next-auth/firebase-adapter@experimental
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import Login from '../components/Forms/Login'
+// import Login from '../components/Forms/Login'
 
 export default function Home() {
   return (
@@ -15,7 +15,7 @@ export default function Home() {
       <h1 className="text-3xl font-bold underline">
         Hello world!
       </h1>
-      <Login />
+      {/* <Login /> */}
     </div>
   )
 }
