31e601231045b96083e470dff54e7dd8421adb4f
Teshane Crawford <crawfordteshane@gmail.com>
2023-02-18 10:25:34 -0500

    Basic setup
    Added frontend tooling for styling and analytics

---
+++ next.config.js
@@ -0,0 +1,13 @@
+const {
+  createVanillaExtractPlugin
+} = require('@vanilla-extract/next-plugin');
+const withVanillaExtract = createVanillaExtractPlugin();
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    appDir: true,
+  },
+}
+
+module.exports = withVanillaExtract(nextConfig);
---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import './styles/globals.css'
+import { AnalyticsWrapper } from '@/components/analytics'
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      {/*
+        <head /> will contain the components returned by the nearest parent
+        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
+      */}
+      <head />
+      <body>
+        {children}
+        <AnalyticsWrapper />
+      </body>
+    </html>
+  )
+}
