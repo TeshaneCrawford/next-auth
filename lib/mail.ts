bba5fec598b75c6ae45dbe342fb9994920bca4ca
Teshane Crawford <crawfordteshane@gmail.com>
2024-01-24 23:33:48 -0500

    Update mail

--- lib/mail.ts
+++ lib/mail.ts
@@ -9,7 +9,7 @@ export const sendTwoFactorTokenEmail = async (
   token: string
 ) => {
   await resend.emails.send({
-    from: "mail@auth-masterclass-tutorial.com",
+    from: "crawfordteshane@gmail.com",
     to: email,
     subject: "2FA Code",
     html: `<p>Your 2FA code: ${token}</p>`
@@ -23,7 +23,7 @@ export const sendPasswordResetEmail = async (
   const resetLink = `${domain}/auth/new-password?token=${token}`
 
   await resend.emails.send({
-    from: "mail@auth-masterclass-tutorial.com",
+    from: "crawfordteshane@gmail.com",
     to: email,
     subject: "Reset your password",
     html: `<p>Click <a href="${resetLink}">here</a> to reset password.</p>`
@@ -37,7 +37,7 @@ export const sendVerificationEmail = async (
   const confirmLink = `${domain}/auth/new-verification?token=${token}`;
 
   await resend.emails.send({
-    from: "mail@auth-masterclass-tutorial.com",
+    from: "crawfordteshane@gmail.com",
     to: email,
     subject: "Confirm your email",
     html: `<p>Click <a href="${confirmLink}">here</a> to confirm email.</p>`
