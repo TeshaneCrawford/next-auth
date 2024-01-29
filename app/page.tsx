1780a0a4cdecefff26e70e18908b3b851afab01a
Teshane Crawford <crawfordteshane@gmail.com>
2024-01-29 12:32:32 -0500

    update page colour

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import { LoginButton } from "@/components/auth/LoginButton";
 
 export default function Home() {
   return (
-    <main className="flex h-full bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-slate-400 to-gray-800">
+    <main className="flex h-full bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-slate-100 to-gray-200">
       <h1>
         Next.js + Next Auth TypeScript Template
       </h1>
